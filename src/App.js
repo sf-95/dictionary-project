840c8ccff966d2ed4174cc9dbda7d0964834f367
sf-95 <sfarch95@gmail.com>
2025-02-22 18:57:38 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import shecodeslogo from "./shecodes-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={shecodeslogo} className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
